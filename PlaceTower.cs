49af7d9906aa90b63e887ad28b3f52b0dbc69148
busyxiang <0111544@kdu-online.com>
2016-06-02 23:58:03 +0800

    Add files via upload

---
+++ PlaceTower.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaceTower : MonoBehaviour {
+
+	public GameObject monsterPrefab;
+	public GameObject monster;
+
+	private bool canPlaceTower(){
+		return monster == null;
+	}
+
+	private bool canUpgradeMonster(){
+		if (monster != null) {
+			MonsterData monsterData = monster.GetComponent<MonsterData> ();
+			MonsterLevel nextLevel = monsterData.GetNextLevel ();
+			if (nextLevel != null) {
+				return true;
+			}
+		}
+		return false;
+		}
+
+	void Update(){
+		
+	}
+
+	void OnMouseUp(){
+		if (!enabled)
+			return;
+		if (canPlaceTower ()) {
+			monster = (GameObject)Instantiate (monsterPrefab, transform.position, Quaternion.identity);
+		}
+		else if (canUpgradeMonster ()) {
+			monster.GetComponent<MonsterData> ().IncreaseLevel ();
+		}
+	}
+
+	void OnMouseOver(){
+		if (canPlaceTower ()) {
+			gameObject.GetComponent<SpriteRenderer> ().color = Color.green;
+		}
+		else{
+			gameObject.GetComponent<SpriteRenderer> ().color = Color.red;
+		}
+	}
+
+	void OnMouseExit(){
+		gameObject.GetComponent<SpriteRenderer> ().color = Color.white;
+	}
+}
