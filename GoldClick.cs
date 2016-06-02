bd6f9fb7a429dbb1a87c4e0bc16693f7bd5a8801
richarddoge8079 <guanrong1226@gmail.com>
2016-06-03 00:02:42 +0800

    Add files via upload

---
+++ GoldClick.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoldClick : MonoBehaviour {
+
+	private GameManagerBehaviour gameManagerCoin;
+
+	// Use this for initialization
+	void Start () {
+		gameManagerCoin = GameObject.Find ("GameManager").GetComponent<GameManagerBehaviour>();
+	}
+	
+	// Update is called once per frame
+	/*void Update () {
+		if (Input.GetMouseButtonUp(0)) {
+			
+		}
+	}*/
+
+	void OnMouseDown () {
+		gameManagerCoin.Gold += 1;
+	}
+}
---
+++ GoldClickPlutos.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoldClickPlutos : MonoBehaviour {
+
+	private GameManagerBehaviour gameManagerPlutos;
+
+	// Use this for initialization
+	void Start () {
+		gameManagerPlutos = GameObject.Find ("GameManager").GetComponent<GameManagerBehaviour>();
+	}
+
+	// Update is called once per frame
+	/*void Update () {
+		if (Input.GetMouseButtonUp(0)) {
+			gameManagerPlutos.Gold += 10;
+		}
+	}*/
+
+	void OnMouseDown () {
+		gameManagerPlutos.Gold += 10;
+	}
+}
