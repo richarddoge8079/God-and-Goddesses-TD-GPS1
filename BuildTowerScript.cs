92ac48c7276922d4dae648275e752681edda6de7
busyxiang <0111544@kdu-online.com>
2016-06-04 12:14:47 +0800

    Add files via upload

---
+++ BuildTowerScript.cs
@@ -0,0 +1,62 @@
+﻿	using UnityEngine;
+using System.Collections;
+
+public class BuildTowerScript : MonoBehaviour {
+
+	public GameObject[] tempObject;
+	public bool activeTower00 = false;
+	public bool activeTower01 = false;
+	public bool activeTower02 = false;
+	public GameObject[] buttonList;
+
+	// Use this for initialization
+	void Start () {
+		buttonList = GameObject.FindGameObjectsWithTag("Button");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Escape))
+			{
+				for(int i=0; i<tempObject.Length; i++)
+				{
+					tempObject[i].GetComponent<PlaceTower>().enabled = false;
+				}
+				activeTower00 = false;
+				activeTower01 = false;
+				activeTower02 = false;
+			}
+	}
+
+	void OnMouseDown()
+	{
+		tempObject = GameObject.FindGameObjectsWithTag("Tile");
+
+		for(int i = 0; i<tempObject.Length; i++)
+		{
+			tempObject[i].GetComponent<PlaceTower>().enabled = true;
+		}
+
+		for(int i =0; i<buttonList.Length; i++)
+		{
+			if(gameObject.name == "Button00")
+			{
+				buttonList[i].GetComponent<BuildTowerScript>().activeTower00 = true;
+				buttonList[i].GetComponent<BuildTowerScript>().activeTower01 = false;
+				buttonList[i].GetComponent<BuildTowerScript>().activeTower02 = false;
+			}
+			if(gameObject.name == "Button01")
+			{
+				buttonList[i].GetComponent<BuildTowerScript>().activeTower01 = true;
+				buttonList[i].GetComponent<BuildTowerScript>().activeTower00 = false;
+				buttonList[i].GetComponent<BuildTowerScript>().activeTower02 = false;
+			}
+			if(gameObject.name == "Button02")
+			{
+				buttonList[i].GetComponent<BuildTowerScript>().activeTower02 = true;
+				buttonList[i].GetComponent<BuildTowerScript>().activeTower01 = false;
+				buttonList[i].GetComponent<BuildTowerScript>().activeTower00 = false;
+			}
+		}
+	}
+}
---
+++ TileScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileScript : MonoBehaviour
+{
+	public enum Type
+	{
+		NONE = 0,
+		PATH,
+		TOTAL
+	}
+
+	public Type type = Type.NONE;
+
+	public Sprite[] spriteList;
+	public SpriteRenderer spriteRenderer;
+
+	void Awake ()
+	{
+		spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
